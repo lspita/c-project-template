533c9a893205be3da8798b7c61c2c1b8a8871c9f
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-10-06 21:50:03 +0200

    compiler warnings

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,9 +1,13 @@
-# project
 set(PROJECT_NAME c-project-template)
 set(PROJECT_VERSION 0.1.0)
 set(PROJECT_DESCRIPTION "C project template")
 set(PROJECT_HOMEPAGE_URL https://github.com/lspita/c-project-template)
 
-# standards
 set(CMAKE_C_STANDARD 99)
 set(CMAKE_CXX_STANDARD 17)
+
+if (MSVC)
+    add_compile_options(/W4)
+else()
+    add_compile_options(-Wall -Wextra -Wpedantic)
+endif()
